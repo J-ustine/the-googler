1be9e028ea655a8da80acdb2821a1615429bbfc3
J-ustine <justine.collache@gmail.com>
2022-01-22 12:49:00 +0100

    Style the create form component

--- src/Form.js
+++ src/Form.js
@@ -1,13 +1,22 @@
 import React from "react";
+import "./Form.css";
 
 export default function Form() {
   return (
-    <form action="">
+    <form action="" className="form-create">
       <h3>New Note</h3>
       <label htmlFor="">Title</label>
-      <input type="text" />
+      <input
+        type="text"
+        placeholder="Enter your Note title"
+        className="createForm-input"
+      />
       <label htmlFor="">Message</label>
-      <textarea></textarea>
+      <textarea
+        placeholder="Enter your note"
+        className="createForm-input"
+      ></textarea>
+      <button className="createForm-button">Create</button>
     </form>
   );
 }
