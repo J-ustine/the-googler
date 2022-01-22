0d1119f690a6e6d77e413351de68722d1e11bba2
J-ustine <justine.collache@gmail.com>
2022-01-22 15:08:21 +0100

    Display login component as home page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="login/userSpace" element={<UserSpace />} />
